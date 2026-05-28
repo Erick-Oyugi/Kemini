0020f9a6fe499b45497113921b98da8b7124290d
Erick Oyugi <erick.oyugi@CreditBank.co.ke>
2026-05-28 08:51:59 +0300

    Initial commit

---
+++ src/netlify/functions/api.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import serverless from "serverless-http";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.json({ message: "Hello from Express on Netlify!" });
+});
+
+app.post("/data", (req, res) => {
+  res.json({
+    received: req.body,
+  });
+});
+
+// Wrap Express app for Netlify
+export const handler = serverless(app);
